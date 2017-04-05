ff7eaf41d20b4e495450ac61c198838e09e576af
Administrator <Administrator@PC-20170329BMCS>
2017-04-05 16:14:50 +0800

    dfgsdfgsdf

---
+++ src/git_test/rgwerg.java
@@ -0,0 +1,27 @@
+/**
+* Copyright ® 2017 北大荒蕙民(北京)食品股份有限公司
+* All right reserved.
+*/ 
+package git_test;
+
+/**
+ * 类描述：
+ * @author：张岩 [zhangyan@haichufang.com]
+ * @since：2017年4月5日 下午4:14:38
+ * @update:[变更日期YYYY-MM-DD][更改人姓名][变更描述]
+ */
+public class rgwerg {
+
+	/**
+	 * 功能描述 ：
+	 * @author：张岩 [zhangyan@haichufang.com]
+	 * @since：2017年4月5日 下午4:14:38
+	 * @update:[变更日期YYYY-MM-DD][更改人姓名][变更描述]
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
